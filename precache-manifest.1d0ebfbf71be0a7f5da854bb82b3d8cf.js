22c7103c08bfdc2cef01d3cdfe452ceecf4bebd3
mosheraDarwesh <moshera.darwesh@black-squares.com>
2020-01-13 16:20:55 +0200

    my build

---
+++ precache-manifest.1d0ebfbf71be0a7f5da854bb82b3d8cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3747c74ad16a376828f74911edd21bc9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e00f402fe10ba8ad7f84",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "26566f9fb3b325698b53",
+    "url": "./static/css/main.62ee928f.chunk.css"
+  },
+  {
+    "revision": "e00f402fe10ba8ad7f84",
+    "url": "./static/js/2.9e4ddc5f.chunk.js"
+  },
+  {
+    "revision": "7ebf6900007f3aece6a94682e6b99f1b",
+    "url": "./static/js/2.9e4ddc5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26566f9fb3b325698b53",
+    "url": "./static/js/main.e7158696.chunk.js"
+  },
+  {
+    "revision": "8c50ee22cd3c70541f6e",
+    "url": "./static/js/runtime-main.acab4f9d.js"
+  },
+  {
+    "revision": "1d61ee3447821612f93ea70e6a27518a",
+    "url": "./static/media/bg.1d61ee34.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spreago/precache-manifest.616a5d44444f5ae06643724f9332f102.js"
+  "./precache-manifest.1d0ebfbf71be0a7f5da854bb82b3d8cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spreago/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
