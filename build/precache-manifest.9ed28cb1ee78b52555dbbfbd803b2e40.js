6aa167dcc0369102860b2994c39d11cd6bd57adb
mosheraDarwesh <moshera.darwesh@black-squares.com>
2020-01-14 10:47:21 +0200

    hope to run

---
+++ build/precache-manifest.9ed28cb1ee78b52555dbbfbd803b2e40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c55083535805c1c410fafae52e2460d",
+    "url": "/spreago-react/index.html"
+  },
+  {
+    "revision": "e00f402fe10ba8ad7f84",
+    "url": "/spreago-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "32b13b292d5a21cd0914",
+    "url": "/spreago-react/static/css/main.afd10961.chunk.css"
+  },
+  {
+    "revision": "e00f402fe10ba8ad7f84",
+    "url": "/spreago-react/static/js/2.9e4ddc5f.chunk.js"
+  },
+  {
+    "revision": "7ebf6900007f3aece6a94682e6b99f1b",
+    "url": "/spreago-react/static/js/2.9e4ddc5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32b13b292d5a21cd0914",
+    "url": "/spreago-react/static/js/main.a3c8d12e.chunk.js"
+  },
+  {
+    "revision": "943fcdd0a905ec6f7d4c",
+    "url": "/spreago-react/static/js/runtime-main.cd45ed29.js"
+  },
+  {
+    "revision": "1d61ee3447821612f93ea70e6a27518a",
+    "url": "/spreago-react/static/media/bg.1d61ee34.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.703bf841e02887079e068b7cabce1246.js"
+  "/spreago-react/precache-manifest.9ed28cb1ee78b52555dbbfbd803b2e40.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spreago-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
