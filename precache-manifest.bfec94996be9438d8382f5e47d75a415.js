62be64ec1f872cf27298addd8b3c0154fa8f6c22
mosheraDarwesh <moshera.darwesh@black-squares.com>
2020-03-22 12:12:04 +0200

    Updates

---
+++ precache-manifest.bfec94996be9438d8382f5e47d75a415.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf7306cc93f4567bae6bb59db5b75651",
+    "url": "/spreago-react/index.html"
+  },
+  {
+    "revision": "e5829fe1f6a75f882ee9",
+    "url": "/spreago-react/static/css/2.f26a4486.chunk.css"
+  },
+  {
+    "revision": "f5b0ef9d1d35d7894382",
+    "url": "/spreago-react/static/css/main.5dedc56c.chunk.css"
+  },
+  {
+    "revision": "e5829fe1f6a75f882ee9",
+    "url": "/spreago-react/static/js/2.8a172652.chunk.js"
+  },
+  {
+    "revision": "f1a729577583dfd1b7e5930e0f34aa67",
+    "url": "/spreago-react/static/js/2.8a172652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b0ef9d1d35d7894382",
+    "url": "/spreago-react/static/js/main.a08c603a.chunk.js"
+  },
+  {
+    "revision": "1c438e62710349e7bb82",
+    "url": "/spreago-react/static/js/runtime-main.fc50af29.js"
+  },
+  {
+    "revision": "7664416fbe452abdbf7e11e1c4c3cd8d",
+    "url": "/spreago-react/static/media/1.7664416f.png"
+  },
+  {
+    "revision": "136b078f702bf93a3ed583c51ff41af6",
+    "url": "/spreago-react/static/media/bg.136b078f.png"
+  },
+  {
+    "revision": "72441ae6c7848ea05c9069bc6b1110d9",
+    "url": "/spreago-react/static/media/john.72441ae6.png"
+  },
+  {
+    "revision": "fbbfed5ae45f03cab2ad8c02b83132e6",
+    "url": "/spreago-react/static/media/member.fbbfed5a.png"
+  },
+  {
+    "revision": "9b677eabbef617fd9311a070beff12ef",
+    "url": "/spreago-react/static/media/overview.9b677eab.svg"
+  },
+  {
+    "revision": "ef5f2dc0a0d3c3e8822d1dac004888aa",
+    "url": "/spreago-react/static/media/publish.ef5f2dc0.svg"
+  },
+  {
+    "revision": "7d2ab46e2108bb358a5ae0bcb8c042e4",
+    "url": "/spreago-react/static/media/question.7d2ab46e.svg"
+  },
+  {
+    "revision": "37039cd65e829e6f3b71f938792e3057",
+    "url": "/spreago-react/static/media/record.37039cd6.svg"
+  },
+  {
+    "revision": "c57837c7a876920b67d98121c87205eb",
+    "url": "/spreago-react/static/media/ux.c57837c7.png"
+  }
+]);
