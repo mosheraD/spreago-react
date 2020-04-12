efa456ada4526d86490878faa75483fe0352f04d
mosheraDarwesh <moshera.darwesh@black-squares.com>
2020-04-12 17:11:25 +0200

    Updates

--- precache-manifest.25370a6fea3c37f1fb0a8507bf59d88c.js
+++ precache-manifest.25370a6fea3c37f1fb0a8507bf59d88c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c090447f9a2dd59f35999100af295d10",
+    "revision": "c2fbcce3ec70b561d41df3747c9d5a63",
     "url": "/spreago-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/spreago-react/static/css/2.f26a4486.chunk.css"
   },
   {
-    "revision": "441141d83b2c07442a78",
+    "revision": "2dbb25326e57391db07d",
     "url": "/spreago-react/static/css/main.a65fefb3.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/spreago-react/static/js/2.dafdc604.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "441141d83b2c07442a78",
-    "url": "/spreago-react/static/js/main.cb126135.chunk.js"
+    "revision": "2dbb25326e57391db07d",
+    "url": "/spreago-react/static/js/main.7976416d.chunk.js"
   },
   {
     "revision": "1c438e62710349e7bb82",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spreago-react/precache-manifest.de8b77502f0c6e4919612214f2872e3c.js"
+  "/spreago-react/precache-manifest.25370a6fea3c37f1fb0a8507bf59d88c.js"
 );
 
 self.addEventListener('message', (event) => {
